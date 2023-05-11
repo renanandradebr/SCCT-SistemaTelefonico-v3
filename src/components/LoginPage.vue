4b343448848b58e15ca2b1597168a4c44a3e40f8
Renan Andrade <85353999+renanandradebr@users.noreply.github.com>
2023-05-11 16:03:25 -0300

    ajustando inputs e centralizando logo do login

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,61 +1,63 @@
 <template>
-    <v-container class="fill-height">
-      <v-row justify="center">
-        <v-col md="4" sm="8">
-          <v-card class="elevation-12">
-            <v-toolbar color="white" dark flat>
-              <v-toolbar-title>
-                <v-img src="../assets/logo-scct.png" width="130" />
-              </v-toolbar-title>
-            </v-toolbar>
-            <v-card-text>
-              <v-form @submit.prevent="Login">
-                <v-text-field v-model="usernamee" label="Nome de Usuário" ></v-text-field>
-                <v-text-field v-model="senha" label="Senha" type="password" variant="outlined"></v-text-field>
-                <v-checkbox v-model="Manterconectado" label="Manter-me Conectado" variant="outlined"></v-checkbox>
-                <v-btn type="submit" color="blue" text-color="white" block variant="outlined">Acessar</v-btn>
-              </v-form>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
+  <v-container class="fill-height">
+    <v-row justify="center">
+      <v-col md="4" sm="8">
+        <v-card class="elevation-12">
+          <v-toolbar color="white" dark flat class="d-flex align-center">        
+              <v-img src="../assets/logo-scct.png"  contain class="imglogo" width="130" />
+          </v-toolbar>
+          <v-card-text>
+            <v-form @submit.prevent="Login">
+              <v-text-field v-model="usernamee" label="Nome de Usuário" outlined clearable></v-text-field>
+              <v-text-field v-model="senha" label="Senha" type="password" variant="outlined" outlined
+                clearable></v-text-field>
+              <v-checkbox v-model="Manterconectado" label="Manter-me Conectado" variant="outlined"></v-checkbox>
+              <v-btn type="submit" color="blue" text-color="white" block variant="outlined">Acessar</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        senha: '',
-        lembrarLogin: false
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      senha: '',
+      lembrarLogin: false
+    }
+  },
+  methods: {
+    Login() {
+
+      if (this.Manterconectado) {
+        localStorage.setItem('lembrarLogin', true);
+      } else {
+        localStorage.removeItem('lembrarLogin');
       }
-    },
-    methods: {
-      Login() {
-        
-        if (this.Manterconectado) {
-          localStorage.setItem('lembrarLogin', true);
-        } else {
-          localStorage.removeItem('lembrarLogin');
-        }
-        // Redireciona o usuário para a página principal após o login
+      // Redireciona o usuário para a página principal após o login
 
 
-        this.$router.push('/home');
-      }
-    },
-    created() {
-      // Carrega o valor da flag de lembrar login do localStorage
-      if (localStorage.getItem('lembrarLogin') === 'true') {
-        this.lembrarLogin = true;
-      }
+      this.$router.push('/home');
+    }
+  },
+  created() {
+    // Carrega o valor da flag de lembrar login do localStorage
+    if (localStorage.getItem('lembrarLogin') === 'true') {
+      this.lembrarLogin = true;
     }
   }
-  </script>
+}
+</script>
   
-  <style scoped>
-  .toolbar {
-    color: #fff;
-  }
-  </style>
+<style scoped>
+.toolbar {
+  color: #fff;
+}
+.imglogo{
+  margin-left: 50%;
+}
+</style>
